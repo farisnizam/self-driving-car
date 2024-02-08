35d14f5620807013485ae3b6921e9352563ec9af
Farisnizam <farismtq@gmail.com>
2024-02-08 15:10:57 +0800

    update sensor to consider traffic

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50,"KEYS");
-const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50,"DUMMY",2)];
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
 
 animate();
 
@@ -21,9 +21,9 @@ function animate() {
 
   road.draw(ctx);
   for (let i = 0; i < traffic.length; i++) {
-    traffic[i].draw(ctx)
+    traffic[i].draw(ctx, "red");
   }
-  car.draw(ctx);
+  car.draw(ctx, "blue");
 
   ctx.restore();
   requestAnimationFrame(animate);
